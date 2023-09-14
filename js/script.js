ded7dfa678e4c571664b9b213ba4ce062fd1beeb
DanderTaes <73030752+DanderTaes@users.noreply.github.com>
2023-09-14 10:28:15 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 10);
+}
+
+a = getRandomInt();
+if (a == 1){
+    window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=XXRg1AtOc9LSlqs4";
+} else{
+    window.location.replace("https://unigow.com/");
+}
+
